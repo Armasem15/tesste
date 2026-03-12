594d75434ccdf4d5372f56ccc0815639fc9357e6
Armasem15 <marcos.walger@escola.pr.gov.br>
2026-03-12 09:06:37 -0300

    Add files via upload

--- bake_continue.js
+++ bake_continue.js
@@ -7,6 +7,10 @@ for (let vaga = 1; vaga <= 10; vaga++) {
         console.log(`Vaga ${vaga}: [INTERDITADA] - pulando...`);
         continue; 
     }
+    if (vaga === 3 || vaga === 7) {
+        console.log(`Vaga ${vaga}: [INTERDITADA] - pulando...`);
+        continue; 
+    }
     carros_estacionados++;
     console.log(`Vaga ${vaga}: [LIVRE] -> Carro estacionado! (Total: ${carros_estacionados})`);
     if (carros_estacionados === limitemaximo) {
